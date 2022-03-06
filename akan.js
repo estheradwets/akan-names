b7c16b4986e0637243ad7907afef9a7a4fc837fb
estheradwets <estheradwets00@gmail.com>
2022-03-07 01:45:29 +0300

    Add background and font colors to alerts in JS

--- akan.js
+++ akan.js
@@ -43,7 +43,7 @@ var submission = function() {
       "Kofi",
       "Kwame"
     ];
-    //Female Akan Names
+    //For Female Akan Names
     var femaleNames = [
       "Akosua",
       " Adwoa",
@@ -54,45 +54,49 @@ var submission = function() {
       "Ama"
     ];
 
-    var month = parseInt(document.getElementById("month").value);
-    var year = parseInt(document.getElementById("year").value);
+    var month = parseInt(document.getElementById("mwezi").value);
+    var year = parseInt(document.getElementById("mwaka").value);
     var day = parseInt(document.getElementById("day").value);
     var name =(document.getElementById("name").value);
     var date0fbirth = new Date(year + "/" + month + "/" + day);
     var results = date0fbirth.getDay();
-    var output = document.getElementById("output");
+    var output = document.getElementById("result");
     var male = document.getElementById("male")
-    var female = document.getElementById("female")
+    var female = document.getElementById("shemale")
 
-    //Possible mistake scenarios
+    //Possible mistake scenarios and the expected response
 
     if (month =="" && year =="" && day =="" && name=="") {
-        alert("Please enter the accurate details");
+        alert("Please enter valid details");
         return false;
     }
-    
+  /* //  For wrong year values, try this 
+ */
 
    if (year < 0) {
       output.style.background ="white"
       output.style.color= "red"
       output.innerHTML = "Oops! " + name + " invalid year of birth! "
   }
-    
+   
+  /* // Invalid month number choices */
+
   else if ((month < 1) || (month > 31)) {
-      output.style.background ="white"
-      output.style.color= "red"
+      output.style.color ="red"
+      output.style.background = "white"
       output.innerHTML = "Oops! " + name + " please enter your valid birth month! "
   }
     
    else if (day < 0 || day > 31) {
+      output.style.color = "red"
       output.style.background ="white"
-      output.style.color= "black"
       output.innerHTML = "Oops! " + name + " please enter a valid day! "
   }
   
+// For true values:
 
     if(male.checked && year > 0 && month > 0 && month < 12 && day > 0 && day < 32) {
-      output.style.background ="blue"
+      output.style.background ="yellowgreen"
         output.innerHTML = "Congrats! " + name + " you were born on a " + daysOfTheWeek[results] + ", your Akan name is " + maleNames[results];
     }
      else if(female.checked && year > 0 && month > 0 && month < 12 && day > 0 && day < 32) {
